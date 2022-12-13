175e7f8ab7dafe716e9e6671ae15797e52d17b1d
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-13 16:49:02 +0100

    fix installationYear with static type

--- Dependencies/General_Functions.py
+++ Dependencies/General_Functions.py
@@ -48,12 +48,15 @@ def read_blob_into_json_array(container_client, blob_name):
     downloaded_blob = container_client.download_blob(blob_name)
 
     # Read the csv-like string into DataFrame
-    df = pd.read_csv(StringIO(downloaded_blob.content_as_text()), na_filter=False)
+    df = pd.read_csv(
+        StringIO(downloaded_blob.content_as_text()),
+        dtype={"InstallationYear": pd.Int64Dtype()},
+    )
 
     # Convert the DataFrame to JSON string
-    json_string = df.to_json(orient="records")
+    json_data = df.to_json(orient="records")
     # Convert the JSON string to JSON object
-    json_array = json.loads(json_string)
+    json_array = json.loads(json_data)
     # Process the json correctly if there are values in map or object format
     for element in json_array:
         for key, value in element.items():
@@ -62,7 +65,7 @@ def read_blob_into_json_array(container_client, blob_name):
             # TODO use DTDL to convert to expected type
             if (
                 isinstance(value, str)
-                and key != 'CriteriaFormula'
+                and key != "CriteriaFormula"
                 and value.startswith("{")
                 and value.endswith("}")
             ):

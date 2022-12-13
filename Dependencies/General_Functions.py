16a3d2b1c3a509764ee5b3b1591f4a5c3d320237
Emmanuel Ruaud <emmanuel.ruaud.ext@cosmotech.com>
2022-12-13 15:01:39 +0100

    restore pandas

--- Dependencies/General_Functions.py
+++ Dependencies/General_Functions.py
@@ -1,16 +1,10 @@
 import os
 import json
 from io import StringIO
-from ast import literal_eval
-from csv import DictReader
-from datetime import datetime
 import time
-import configparser
-import sys
-import math
+import pandas as pd
 from azure.storage.queue import QueueClient
 from azure.core.exceptions import ResourceNotFoundError
-
 
 def ls_files(client, path, recursive=False):
     """
@@ -50,36 +44,29 @@ def read_blob_into_json_array(container_client, blob_name):
     Reads a blob from a container and stores its content into a json array
     in which every element represents a line in the the blob
     """
-    """
-    Reads a blob from a container and stores its content into a json array
-    in which every element represents a line in the the blob
-    """
     downloaded_blob = container_client.download_blob(blob_name)
 
     # Read the csv-like string into DataFrame
-    csv_data = DictReader(StringIO(downloaded_blob.content_as_text()))
-    data = list(csv_data)
+    df = pd.read_csv(StringIO(downloaded_blob.content_as_text()), na_values="")
+
+    # Convert the DataFrame to JSON string
+    json_string = df.to_json(orient="records")
+    # Convert the JSON string to JSON object
+    json_array = json.loads(json_string)
     # Process the json correctly if there are values in map or object format
-    for element in data:
+    for element in json_array:
         for key, value in element.items():
             # condition on 'CriteriaFormula is specifec for asset
             # this field contain condition formula which mustn't be convert to json
             # TODO use DTDL to convert to expected type
             if (
-                value.startswith("{")
+                isinstance(value, str)
+                and key != 'CriteriaFormula'
+                and value.startswith("{")
                 and value.endswith("}")
-                and key != "CriteriaFormula"
             ):
-                try:
-                    element[key] = json.loads(value.replace(";", ","))
-                    continue
-                except json.JSONDecodeError:
-                    pass
-            try:
-                element[key] = literal_eval(value)
-            except Exception:
-                pass
-    return data
+                element[key] = json.loads(value.replace(";", ","))
+    return json_array
 
 
 def wait_end_of_queue(queue_client: QueueClient):
